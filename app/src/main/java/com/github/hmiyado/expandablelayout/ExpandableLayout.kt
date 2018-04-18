fc57c8ce7d365b7c0089fe361f4e47f0642422a1
hmiyado <hmiyado@gmail.com>
2018-04-18 11:43:36 +0900

    animate on expanding/shrinking

--- app/src/main/java/com/github/hmiyado/expandablelayout/ExpandableLayout.kt
+++ app/src/main/java/com/github/hmiyado/expandablelayout/ExpandableLayout.kt
@@ -2,7 +2,10 @@ package com.github.hmiyado.expandablelayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.FrameLayout
+import android.widget.Scroller
 
 /**
  * Created by hmiyado on 2018/04/12.
@@ -10,13 +13,14 @@ import android.widget.FrameLayout
  * 開いたり閉じたりできるレイアウト
  */
 class ExpandableLayout @JvmOverloads constructor(
-    context: Context,
-    attributeSet: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-    defStyleRes: Int = 0
+        context: Context,
+        attributeSet: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0
 ) : FrameLayout(context, attributeSet, defStyleAttr, defStyleRes) {
     private val shrinkWidthRatio: Float
     private val shrinkHeightRatio: Float
+    private val scroller: Scroller
     var state: State = State.Expand
         private set
 
@@ -25,6 +29,8 @@ class ExpandableLayout @JvmOverloads constructor(
         shrinkWidthRatio = attrs.getFloat(R.styleable.ExpandableLayout_exp_shrink_width, 1f)
         shrinkHeightRatio = attrs.getFloat(R.styleable.ExpandableLayout_exp_shrink_height, 1f)
         attrs.recycle()
+
+        scroller = Scroller(context, AccelerateDecelerateInterpolator())
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -34,28 +40,53 @@ class ExpandableLayout @JvmOverloads constructor(
         val heightMeasure = MeasureSpec.getMode(heightMeasureSpec)
         val heightSize = MeasureSpec.getSize(heightMeasureSpec)
 
+        Log.d("onMeasure", "scroller ${scroller.isFinished} ${scroller.currX} ${scroller.currY}")
+        if (scroller.computeScrollOffset()) {
+            super.onMeasure(
+                    MeasureSpec.makeMeasureSpec(scroller.currX, widthMeasure)
+                    , MeasureSpec.makeMeasureSpec(scroller.currY, heightMeasure))
+            return
+        }
+
         when (state) {
-            ExpandableLayout.State.Expand -> super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            ExpandableLayout.State.Expand -> {
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            }
             ExpandableLayout.State.Shrink -> super.onMeasure(
-                MeasureSpec.makeMeasureSpec(
-                    (widthSize * shrinkWidthRatio).toInt(),
-                    widthMeasure
-                ),
-                MeasureSpec.makeMeasureSpec(
-                    (heightSize * shrinkHeightRatio).toInt(),
-                    heightMeasure
-                )
+                    MeasureSpec.makeMeasureSpec(
+                            (widthSize * shrinkWidthRatio).toInt(),
+                            widthMeasure
+                    ),
+                    MeasureSpec.makeMeasureSpec(
+                            (heightSize * shrinkHeightRatio).toInt(),
+                            heightMeasure
+                    )
             )
         }
     }
 
     fun expand() {
         state = State.Expand
+        scroller.forceFinished(true)
+        scroller.startScroll(measuredWidth, measuredHeight, (measuredWidth * (1 - shrinkWidthRatio)).toInt(), (measuredHeight * (1 - shrinkHeightRatio)).toInt())
+        scroll()
         requestLayout()
+    }
+
+    private fun scroll() {
+        handler.postDelayed({
+            if (!scroller.isFinished) {
+                requestLayout()
+                scroll()
+            }
+        }, 10)
     }
 
     fun shrink() {
         state = State.Shrink
+        scroller.forceFinished(true)
+        scroller.startScroll(measuredWidth, measuredHeight, -(measuredWidth * (1 - shrinkWidthRatio)).toInt(), -(measuredHeight * (1 - shrinkHeightRatio)).toInt())
+        scroll()
         requestLayout()
     }
 
